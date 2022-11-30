16a027a5a679a3bd0d076aeb99eddc395b3d6c79
p65454 <106226135+p65454@users.noreply.github.com>
2022-11-30 22:41:41 +0900

    1130test
    test

--- user/serializers.py
+++ user/serializers.py
@@ -1,49 +1,39 @@
-# from .models import User
-# from rest_framework import serializers
-# class RegisterSerializer(serializers.ModelSerializer):
-#     def create(self, validated_data):
-#         user = User.objects.create_user(
-#             email = validated_data['email'],
-#             name = validated_data['name'],
-#             login_id = validated_data['login_id'],
-#             password = validated_data['password'],
-
-#             age = validated_data['age'],
-#             gender = validated_data['gender'],
-#             phone_number = validated_data['phone_number'],
-#         )
-#         return user
-#     class Meta:
-#         model = User
-#         fields = ['login_id', 'email', 'name', 'password', 'age', 'gender', 'phone_number']
-
-# class UserSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = User
-#         fields = '__all__'
 from .models import User
 
 from rest_framework import serializers
 from rest_framework_simplejwt.tokens import RefreshToken
 from user.models import User
+
 class SignupSirializer(serializers.ModelSerializer):
     email = serializers.EmailField(
         required = True,
     ),
     password = serializers.CharField(
-        required=True,
+        required = True,
         write_only = True,
+        style = {'input_type': 'password'} 
     )
-    password2 = serializers.CharField(write_only = True, required=True)
+    password2 = serializers.CharField(
+        write_only = True,
+        required = True,
+        style = {'input_type': 'password'}
+    )
     
     class Meta:
         model = User
         fields = ('login_id','password', 'password2', 'name','email','age','gender','phone_number')
     
     def validate(self, data):
+        
+             
+        if User.objects.filter(login_id=data['login_id']).exists():
+            raise serializers.ValidationError({
+                'login_id': '이미 존재하는 아이디입니다.'
+                })
+
         if data['password'] != data['password2']:
             raise serializers.ValidationError({
-                "password" : "Pass word fields didn't match"
+                "password" : "비밀번호가 맞지 않습니다."
             })
         
         return data
